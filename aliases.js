909b3127812892ea9e8169b8b871933a303d6e0e
Travis CI User <travis@example.org>
2018-11-15 21:03:18 +0000

    Documentation for alexcrichton/git2-rs@8002f97

---
+++ aliases.js
@@ -0,0 +1,4 @@
+var ALIASES = {};
+ALIASES['git2'] = {};
+ALIASES['git2_curl'] = {};
+ALIASES['libgit2_sys'] = {};
---
+++ implementors/core/convert/trait.Into.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["git2"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.Into.html\" title=\"trait core::convert::Into\">Into</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.i32.html\">i32</a>&gt; for <a class=\"enum\" href=\"git2/enum.TreeWalkResult.html\" title=\"enum git2::TreeWalkResult\">TreeWalkResult</a>",synthetic:false,types:["git2::tree::TreeWalkResult"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.Into.html\" title=\"trait core::convert::Into\">Into</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.u32.html\">u32</a>&gt; for <a class=\"enum\" href=\"git2/enum.TreeWalkMode.html\" title=\"enum git2::TreeWalkMode\">TreeWalkMode</a>",synthetic:false,types:["git2::tree::TreeWalkMode"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
